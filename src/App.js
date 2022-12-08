f739f6701e190f7390249932ad0a2c9d220383a9
scsigma <sullyclark@clarks01-sigma.local>
2022-12-08 15:58:42 -0800

    trying apexcharts

--- src/App.js
+++ src/App.js
@@ -28,20 +28,84 @@
 
 // I want to use Hooks if I can, possibly
 
-import React, { useState, useMemo, useRef } from 'react';
+import React, { 
+  useState, 
+  useEffect,
+  useMemo, 
+  useRef } from 'react';
 
 import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
-// import 
-
+// import Highcharts from 
+import ApexCharts from 'apexcharts';
+import Chart from 'react-apexcharts';
 import { client, useConfig, useElementData } from "@sigmacomputing/plugin";
 
+var ser = {
+      
+  series: [
+  {
+    name: 'Manufacturing',
+    data: [
+      {
+        x: 'Project A',
+        y: [
+          new Date('2019-03-05T12:00:00Z').getTime(),
+          new Date('2019-03-08T12:00:00Z').getTime()
+        ]
+      },
+      // {
+      //   x: 'Project A',
+      //   y: [
+      //     new Date('2019-03-10').getTime(),
+      //     new Date('2019-03-12').getTime()
+      //   ]
+      // },
+    ]
+  }],
+  options: {
+    chart: {
+      height: 450,
+      type: 'rangeBar'
+    },
+    plotOptions: {
+      bar: {
+        horizontal: true,
+        barHeight: '80%'
+      }
+    },
+    xaxis: {
+      type: 'datetime'
+      // labels: {
+      //   format: 'dd/MM'
+      // }
+    },
+    stroke: {
+      width: 1
+    },
+    fill: {
+      type: 'solid',
+      opacity: 0.6
+    },
+    // legend: {
+    //   position: 'top',
+    //   horizontalAlign: 'left'
+    // }
+  },
+}
 
 function App() {
+
+  const [dog, setDog] = useState(ser);
+
+  useEffect(() => {
+  setDog(ser);
+  }, [])
 
   return (
     <p>
       hello
+      <Chart options={dog.options} series={dog.series} type="rangeBar" height={450}  />
     </p>
   )
 }
