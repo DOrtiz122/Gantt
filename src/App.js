4a6ebee007a7efe99f5f51057d0643deadba5d13
scsigma <sullyclark@clarks01-sigma.local>
2022-12-12 18:03:18 -0800

    debugging

--- src/App.js
+++ src/App.js
@@ -194,6 +194,8 @@ const App = () => {
           // can convert the string to a number and back to an int. if same value, it is wono. if not, it is operation.
           // convert string # to int
           var temp = parseInt(first_val);
+          console.log('temp to string ', temp.toString());
+          console.log('first val ', first_val[0]);
           if (first_val[0] === temp.toString()) {
             // this is wono bc the values are the same before and after, meaning it was an int
             sigmaObj.wono = sigmaData[config.dimension[i]];
