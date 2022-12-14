5b03bc5e53077c1738e685739f5d78de4b616b82
scsigma <sullyclark@clarks01-sigma.local>
2022-12-14 12:44:01 -0800

    refactor attempt 4

--- src/App.js
+++ src/App.js
@@ -54,37 +54,44 @@ const App = () => {
   // object builder and array builder functions
   // build data object first
   const sigmaObjectBuilder = (sigmaData, config) => {
+    const dimensions = config.dimension;
+    const measures = config.measures;
+
 
     let obj = {};
-    for (var i = 0; i < config.dimension.length; i++) {
-      var first_val = sigmaData[config.dimension[i]][0];
-      if (typeof first_val !== 'string') {
-        // this is start_times
-        obj.start_time = sigmaData[config.dimension[i]];
-      } else {
-        // it is either wono or operation
-        // can convert the string to a number and back to an int. if same value, it is wono. if not, it is operation.
-        // convert string # to int
-        var temp = parseInt(first_val);
-        if (first_val === '00' + temp.toString()) {
-          // this is wono bc the values are the same before and after, meaning it was an int
-          obj.wono = sigmaData[config.dimension[i]];
+
+    if (dimensions && measures) {
+      for (var i = 0; i < config.dimension.length; i++) {
+        var first_val = sigmaData[config.dimension[i]][0];
+        if (typeof first_val !== 'string') {
+          // this is start_times
+          obj.start_time = sigmaData[config.dimension[i]];
         } else {
-          // not the same before and after, therefor it was a string operation
-          obj.operation = sigmaData[config.dimension[i]];
+          // it is either wono or operation
+          // can convert the string to a number and back to an int. if same value, it is wono. if not, it is operation.
+          // convert string # to int
+          var temp = parseInt(first_val);
+          if (first_val === '00' + temp.toString()) {
+            // this is wono bc the values are the same before and after, meaning it was an int
+            obj.wono = sigmaData[config.dimension[i]];
+          } else {
+            // not the same before and after, therefor it was a string operation
+            obj.operation = sigmaData[config.dimension[i]];
+          }
         }
       }
+      // add the end time array as well, which is the only measure
+      obj.end_time = sigmaData[config.measures[0]];
     }
 
-    // add the end time array as well, which is the only measure
-    obj.end_time = sigmaData[config.measures[0]];
+
 
     return obj;
   }
 
   // process data object to be series array
   var sigmaSeriesBuilder = (obj) => {
-    // debugger;
+    
     let arr = [];
   
     let i = 0;
@@ -93,69 +100,70 @@ const App = () => {
     // declare prev_wono outside
     let prev_wono;
     let newObj;
+    if (obj !== {}) {
+      while (i < obj.wono.length) {
+  
+        // base case for the first wono
+        if (!prev_wono) {
+          prev_wono = obj.wono[i];
+          newObj = {
+            name: prev_wono,
+            data: [],
+            // maybe remove below
+            // y: wono_count
+          }
     
-    while (i < obj.wono.length) {
-  
-      // base case for the first wono
-      if (!prev_wono) {
-        prev_wono = obj.wono[i];
-        newObj = {
-          name: prev_wono,
-          data: [],
-          // maybe remove below
-          // y: wono_count
+          // I also want to add to this data array the parent object
+          newObj.data.push({
+            name: prev_wono,
+            id: 'wono-' + wono_count.toString(),
+            pointWidth: 3,
+            // y: wono_count
+          })
         }
-  
-        // I also want to add to this data array the parent object
-        newObj.data.push({
-          name: prev_wono,
-          id: 'wono-' + wono_count.toString(),
-          pointWidth: 3,
-          // y: wono_count
-        })
+        
+        let curr_wono = obj.wono[i];
+        // if prev wono and curr_wono are different,
+        // we are at a new wono
+        if (prev_wono !== curr_wono) {
+          wono_count++;
+          arr.push(newObj);
+          newObj = {
+            name: curr_wono,
+            data: [],
+            // maybe remove below
+            // y: wono_count
+          }
+          // reset prev_wono
+          prev_wono = curr_wono;
+    
+          // Add the parent object to newObj data array
+          newObj.data.push({
+            name: curr_wono,
+            id: 'wono-' + wono_count.toString(),
+            pointWidth: 3,
+            // y: wono_count
+          })
+        } else {
+          // we are in the same wono still
+          // so we want to add to the data key
+          let dataObj = {
+            parent: 'wono-' + wono_count.toString(),
+            name: obj.operation[i],
+            start: obj.start_time[i],
+            end: obj.end_time[i],
+            // y: wono_count
+          }
+          // add the new data object to the array
+          newObj.data.push(dataObj);
+          i++;
+        }
       }
-      
-      let curr_wono = obj.wono[i];
-      // if prev wono and curr_wono are different,
-      // we are at a new wono
-      if (prev_wono !== curr_wono) {
-        wono_count++;
-        arr.push(newObj);
-        newObj = {
-          name: curr_wono,
-          data: [],
-          // maybe remove below
-          // y: wono_count
-        }
-        // reset prev_wono
-        prev_wono = curr_wono;
-  
-        // Add the parent object to newObj data array
-        newObj.data.push({
-          name: curr_wono,
-          id: 'wono-' + wono_count.toString(),
-          pointWidth: 3,
-          // y: wono_count
-        })
-      } else {
-        // we are in the same wono still
-        // so we want to add to the data key
-        let dataObj = {
-          parent: 'wono-' + wono_count.toString(),
-          name: obj.operation[i],
-          start: obj.start_time[i],
-          end: obj.end_time[i],
-          // y: wono_count
-        }
-        // add the new data object to the array
-        newObj.data.push(dataObj);
-        i++;
-      }
+    
+      // push last newObj to the arr
+      arr.push(newObj)
     }
-  
-    // push last newObj to the arr
-    arr.push(newObj)
-  
+    
     return arr;
   }
 
